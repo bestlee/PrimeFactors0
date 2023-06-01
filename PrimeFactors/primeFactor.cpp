c30f330524ea7f7ab47db878653276f0355b1358
Myunghee <audgml0087@naver.com>
2023-06-01 16:25:31 +0900

    [refactoring] 59p

--- PrimeFactors/primeFactor.cpp
+++ PrimeFactors/primeFactor.cpp
@@ -7,27 +7,14 @@ public:
 	{
 		std::vector<int> result = {};
 
-		if (number > 1)
+		for (int divisor = 2; number > 1; divisor++)
 		{
-			int divisor = 2;
-			if (number ==6 || number==9 || number == 4 || number==12)
+			while (number % divisor == 0)
 			{
-				for (divisor =2; number >1; divisor++)
-				{
-					while (number % divisor == 0)
-					{
-						result.push_back(divisor);
-						number /= divisor;
-					}
-				}
+				result.push_back(divisor);
+				number /= divisor;
 			}
-			else
-			{
-				result.push_back((number));
-			}
-
 		}
-
 		return result;
 	}
 };
