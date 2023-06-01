9d6cdb616a06cc225da8eda8fba2c922023925b1
Myunghee <audgml0087@naver.com>
2023-06-01 15:51:59 +0900

    [feature] 40p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -16,9 +16,10 @@ TEST_F(PrimeFixture, Of1) {
 }
 
 TEST_F(PrimeFixture, Of2) {
-
 	expected = {2};
-
 	EXPECT_EQ(expected, prime_factor.of(2));
- 
+}
+TEST_F(PrimeFixture, Of3) {
+	expected = {3};
+	EXPECT_EQ(expected, prime_factor.of(3));
 }
