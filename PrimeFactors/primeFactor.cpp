013d4afc3b6badab5eed01e4d7a7d3caf4fa2a71
Myunghee <audgml0087@naver.com>
2023-06-01 15:43:57 +0900

    [feature] 37p

--- PrimeFactors/primeFactor.cpp
+++ PrimeFactors/primeFactor.cpp
@@ -6,6 +6,11 @@ public:
 	std::vector<int> of(int i)
 	{
 		std::vector<int> result = {};
+
+		if(i == 2)
+		{
+			result.push_back((2));
+		}
 		return result;
 	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -8,3 +8,11 @@ TEST(PrimeFactors, Of1) {
 	EXPECT_EQ(expected, prime_factor.of(1));
  
 }
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor prime_factor;
+	std::vector<int> expected = {2};
+
+	EXPECT_EQ(expected, prime_factor.of(2));
+ 
+}
